05c11d5b88243c721718a416e9e31811633cce1d
Dorin Cazan <cazandorin2002@gmail.com>
2021-04-01 20:32:01 +0300

    az

--- Atestat/Form4.Designer.cs
+++ Atestat/Form4.Designer.cs
@@ -33,6 +33,7 @@ namespace Atestat
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Istoric_text
@@ -75,11 +76,21 @@ namespace Atestat
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(273, 444);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Ghencea";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // F4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 474);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -97,5 +108,6 @@ namespace Atestat
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
